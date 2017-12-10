8c8b24ef0c7b81036e3f39a87dfc4e667889905f
One project <esaul-00@mail.ru>
2017-12-11 00:24:39 +0300

    add file x.js

---
+++ my_scripts/functions.js
@@ -0,0 +1,31 @@
+/*function inp() {
+  //input type = "text" value ="abc"
+  <p><input type="text" value="abc"> (text)</p>
+
+}
+*/
+/*
+function match1() {
+  var intp1,intp2;
+  intp1 = 10;
+}
+*/
+/*
+1- нужно получить данные котировок с сайта ехмо
+2- дабавить "+" ,при нажатии на который будет выпадать список требуемой пары валют
+3- создать панели с заголовком пары котировок
+4- в панели валюты создать max/min текущий курс валюты,цена покупки,профит
+5-
+*/
+
+
+function someFunc(){
+    var el = document.getElementById('rezult');
+    el.value = 1231321;
+}
+function hello() {
+  var s = 'hello';
+  console.log(s);
+  return s
+
+}
